eda87ef0f6edc80f2a07700cb970e98b23180973
Dhrumil Shah <dhrumil.ketul@gmail.com>
2022-06-06 17:17:03 -0600

    update

--- archived protocols/protocol 1/protocol.sol
+++ archived protocols/protocol 1/protocol.sol
@@ -14,3 +14,39 @@ abstract contract IRewardDistributionRecipient is Ownable {
         rewardDistribution = _rewardDistribution;
     }
 }
+
+pragma solidity 0.6.8;
+
+contract LPTokenWrapper {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable uni;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    constructor(IERC20 uni_) public {
+        uni = uni_;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function stake(uint256 amount) public virtual {
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        uni.safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(uint256 amount) public virtual {
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        uni.safeTransfer(msg.sender, amount);
+    }
+}
